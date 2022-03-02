3e0020e05714e9933cf00ad2125429be247c27ed
ransayada <ran.sayada@gmail.com>
2022-03-02 11:48:16 +0200

    first commit

--- src/app/cmps/count-down/count-down.component.ts
+++ src/app/cmps/count-down/count-down.component.ts
@@ -10,34 +10,23 @@ export class CountDownComponent implements OnInit {
 
   @Input() targetTime: any;
   @Output() due = new EventEmitter();
-  timeOut = Date.now();
-  timeoutId: ReturnType<typeof setInterval> | null = null;
+  timeoutId: any;
   timeDiff = 0;
 
-  showMinutes() {
-    const min = Math.floor(this.timeDiff / 60000);
-    if (this.timeDiff <= 0) return '00';
-    return min.toString().padStart(2, '0');
-  }
-
-  showSeconds() {
-    const sec = ((this.timeDiff % 60000) / 1000).toFixed(0);
-    if (this.timeDiff <= 0) return '00';
-    else return sec.toString().padStart(2, '0');
-  }
-
-  showRed() {
-    return Math.floor(this.timeDiff / 1000) <= 5;
-  }
-
   ngOnInit(): void {
-    this.timeoutId = setInterval(() => {
-      this.timeOut += 1000;
-      this.timeDiff = this.targetTime - this.timeOut;
-      if (this.timeDiff <= 0) {
-        clearInterval(this.timeoutId as ReturnType<typeof setInterval>);
+    this.timeDiff = this.targetTime - Date.now();
+    this.timeoutId = window.setInterval(() => {
+
+      this.timeDiff = this.targetTime - Date.now();
+      if (this.timeDiff <= 1000) {
+        clearInterval(this.timeoutId);
         this.due.emit();
       }
     }, 1000);
+  }
+
+
+  get isRed() {
+    return Math.floor(this.timeDiff / 1000) <= 5;
   }
 }
